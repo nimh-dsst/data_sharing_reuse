536172d7b064c094f46c44985f8a2ba665fda6de
Travis Riddle <riddleta@promethium.nimh.nih.gov>
2020-02-24 10:47:31 -0500

    first real commit for this file (before was code from cookiecutter template)

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,44 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+def return_passages(r_out):
+    out_dat = []
+    
+    try:
+        tmp_doi = r_out['documents'][0]['passages'][0]['infons']['article-id_doi']
+    except:
+        tmp_doi = None
+    try:
+        tmp_pmcid = r_out['documents'][0]['id']
+    except:
+        tmp_pmcid = None
+        
+    for passage in r_out['documents'][0]['passages']:
+        if test_suitability:
+            try:
+                section_type = passage['infons']['section_type']
+            except:
+                section_type=None
+                
+            out_dat.append([passage['text'],
+                           passage['offset'],
+                           tmp_pmcid,
+                           tmp_doi,
+                           section_type])
+        
+    return(out_dat)
 
+def test_suitability(passage):
+    #figures out whether a passage is suitable for what we're after.
+    #probably still needs some fiddling.
+    suitability = True
+    try:
+        if passage['infons']['section_type'].lower() == 'ref':
+            suitability = False
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+        if passage['infons']['type'].lower() == 'table':
+            suitability = False
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+        if 'title' in passage['infons']['type'].lower():
+            suitability = False
+    except:
+        suitability = False
+    
+    return(suitability)
